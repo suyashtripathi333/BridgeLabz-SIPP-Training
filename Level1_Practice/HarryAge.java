20e1d59a4f3af09783632b37e1136455cc248117
suyashtripathi333 <suyash.333.tripathi@gmail.com>
2025-06-13 10:38:02 +0530

    Second commit

---
+++ Level1_Practice/HarryAge.java
@@ -0,0 +1,11 @@
+package Level1_Practice;
+
+
+public class HarryAge {
+    public static void main(String[] args) {
+        int birthYear = 2000;
+        int currentYear = 2024;
+        int age = currentYear - birthYear;
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
---
+++ Level1_Practice/KmTOMiles.java
@@ -0,0 +1,10 @@
+package Level1_Practice;
+
+
+public class KmToMiles {
+    public static void main(String[] args) {
+        double km = 10.8;
+        double miles = km / 1.6;
+        System.out.println("The distance " + km + " km in miles is " + miles);
+    }
+}
