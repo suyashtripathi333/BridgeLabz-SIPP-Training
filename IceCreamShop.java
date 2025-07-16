4c316922305856b2877df2b2b73b32ee777e4d17
suyashtripathi333 <suyash.333.tripathi@gmail.com>
2025-07-16 10:28:52 +0530

    [Suyash]Add. Added Ice Cream Parlour

---
+++ IceCreamShop.java
@@ -0,0 +1,64 @@
+
+
+class IceCream{
+	String flavour;
+	int netsales;
+	
+	IceCream(String flavour, int netsales){
+		this.flavour = flavour;
+		this.netsales = netsales;
+	}
+	
+	void display() {
+        System.out.println("Flavour: " + flavour + ", Net Sales: " + netsales);
+	}
+}
+
+class IceCreamParlour{
+	
+	void sortBySales(IceCream[] arr) {
+		int n = arr.length;
+		
+		
+		for(int i =0; i<n-1;i++) {
+			for(int j =0; j<n-i-1;j++) {
+				if(arr[j].netsales<arr[j+1].netsales) {
+					IceCream temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+				}
+			}
+		}
+	}
+	
+	 void displayAll(IceCream[] arr) {
+	        for (int i = 0; i < arr.length; i++) {
+	            arr[i].display();
+	        }
+	    }
+}
+public class IceCreamShop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 // Create ice cream flavors manually (no collections)
+        IceCream[] flavours = new IceCream[5];
+        flavours[0] = new IceCream("Vanilla", 120);
+        flavours[1] = new IceCream("Chocolate", 200);
+        flavours[2] = new IceCream("Mango", 180);
+        flavours[3] = new IceCream("Strawberry", 90);
+        flavours[4] = new IceCream("Butterscotch", 150);
+
+        IceCreamParlour parlour = new IceCreamParlour();
+
+        System.out.println("Before Sorting:");
+        parlour.displayAll(flavours);
+
+        parlour.sortBySales(flavours);
+
+        System.out.println("\nAfter Sorting by Net Sales (Descending):");
+        parlour.displayAll(flavours);
+
+	}
+
+}
